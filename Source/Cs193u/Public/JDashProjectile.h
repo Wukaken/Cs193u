b189036fbeee10b3b368b94c350e1505fdccf3fb
zerowjj <zerowjj@gmail.com>
2021-03-30 19:04:28 +0800

    update assignment 2

---
+++ Source/Cs193u/Public/JDashProjectile.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "JDashProjectile.generated.h"
+
+class USphereComponent;
+class UStaticMeshComponent;
+class UParticleSystemComponent;
+class UPrimitiveComponent;
+
+UCLASS()
+class CS193U_API AJDashProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AJDashProjectile();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Projectile")
+	USphereComponent* CollisionComp;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Mesh")
+	UStaticMeshComponent* MeshComp;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Fx")
+	UParticleSystemComponent* FxComp;
+
+public:	
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, 
+			   UPrimitiveComponent* OtherComp, FVector NormalImpulse,
+			   const FHitResult& Hit);
+
+	void Teleport();
+
+};
